c0f7a34dab53e36b3f6dfcc78beac7cb8b1181ef
saritha <144457175+sarithasankari@users.noreply.github.com>
2026-06-02 18:33:16 +0530

    Debug build-1

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Current directory:"
-pwd
-
-echo "Files:"
-ls
-
-echo "Requirements:"
-cat requirements.txt
-
 python manage.py collectstatic --no-input
